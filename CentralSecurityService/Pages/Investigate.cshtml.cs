16406ebdb09c8c71ef0ca6c300be33921653d68a
Eamonn Duffy <Eamonn@Duffy.global>
2025-06-06 01:44:20 +0100

    feat: First Attempt at ASP.NET Central Security Service Web Site Source.

---
+++ CentralSecurityService/Pages/Investigate.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CentralSecurityService.Pages
+{
+    public class InvestigateModel : PageModel
+    {
+        public async Task OnGetAsync()
+        {
+        }
+    }
+}
