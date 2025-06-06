dc60815429ed2126ec96a0fbf1c336dc6acab945
Eamonn Duffy <Eamonn@Duffy.global>
2025-06-06 20:02:05 +0100

    feat: Added Eadent.Common.

---
+++ CentralSecurityService/Configuration/CentralSecurityServiceSettings.cs
@@ -0,0 +1,46 @@
+﻿using static CentralSecurityService.Configuration.CentralSecurityServiceSettings;
+
+namespace CentralSecurityService.Configuration
+{
+    public class CentralSecurityServiceSettings
+    {
+        public const string SectionName = "CentralSecurityService";
+
+        public static CentralSecurityServiceSettings Instance { get; private set; }
+
+        public CentralSecurityServiceSettings()
+        {
+            Instance = this;
+        }
+
+        public class DatabaseSettings
+        {
+            public string DatabaseServer { get; set; }
+
+            public string DatabaseName { get; set; }
+
+            public string DatabaseSchema { get; set; }
+
+            public string ApplicationName { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Password { get; set; }
+
+            public string ConnectionString => $"Server={DatabaseServer};Database={DatabaseName};Application Name={ApplicationName};User Id={UserName};Password={Password};Encrypt=false;";
+        }
+
+        public class GoogleReCaptchaSettings
+        {
+            public string SiteKey { get; set; }
+
+            public string Secret { get; set; }
+
+            public decimal MinimumScore { get; set; }
+        }
+
+        public DatabaseSettings Database { get; set; }
+
+        public GoogleReCaptchaSettings GoogleReCaptcha { get; set; }
+    }
+}
