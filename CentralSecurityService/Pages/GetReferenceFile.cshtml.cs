360ada796a823c7192e71fefb26ebc397f52476a
Eamonn Duffy <Eamonn@Duffy.global>
2025-06-23 20:42:54 +0100

    feat: Reference Files that are Redacted can now not be accessed.

--- CentralSecurityService/Pages/GetReferenceFile.cshtml.cs
+++ CentralSecurityService/Pages/GetReferenceFile.cshtml.cs
@@ -1,3 +1,5 @@
+using CentralSecurityService.Common.DataAccess.CentralSecurityService.Entities;
+using CentralSecurityService.Common.DataAccess.CentralSecurityService.Repositories;
 using CentralSecurityService.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,16 +13,38 @@ namespace CentralSecurityService.Pages
 
         private IWebHostEnvironment WebHostEnvironment { get; }
 
-        public GetReferenceFileModel(ILogger<GetReferenceFileModel> logger, IWebHostEnvironment webHostEnvironment)
+        private IReferencesRepository ReferencesRepository { get; }
+
+        public GetReferenceFileModel(ILogger<GetReferenceFileModel> logger, IWebHostEnvironment webHostEnvironment, IReferencesRepository referencesRepository)
         {
             Logger = logger;
             WebHostEnvironment = webHostEnvironment;
+            ReferencesRepository = referencesRepository;
         }
 
-        public async Task<IActionResult> OnGetAsync(string referenceFile)
+        public async Task<IActionResult> OnGetAsync(string type, string referenceFile)
         {
-            if (string.IsNullOrEmpty(referenceFile))
-                return BadRequest("Reference File name cannot be null or empty.");
+            if (string.IsNullOrWhiteSpace(type))
+                return BadRequest("Type name cannot be null or whitespace.");
+
+            if (string.IsNullOrWhiteSpace(referenceFile))
+                return BadRequest("Reference File name cannot be null or whitespace.");
+
+            ReferenceEntity referenceEntity = null;
+
+            if (type == "Thumbnail")
+            {
+                referenceEntity = ReferencesRepository.GetLastOrDefault(where => where.ThumbnailFileName == referenceFile, orderBy => orderBy.ReferenceId);
+            }
+            else if (type == "Full")
+            {
+                referenceEntity = ReferencesRepository.GetLastOrDefault(where => where.ReferenceName == referenceFile, orderBy => orderBy.ReferenceId);
+            }
+
+            if ((referenceEntity == null) || ((referenceEntity != null) && referenceEntity.Redacted))
+            {
+                return NotFound("The Reference File is Invalid or Redacted.");
+            }
 
             string referenceFilesFolder = null;
 
